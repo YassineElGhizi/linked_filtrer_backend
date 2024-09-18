7b6c93e8d2bff40d0258b8ab642b7159df82b481
yassine_el_ghizi <yassine.elghizi@smarteez.eu>
2024-09-18 19:37:04 +0100

    forcing migrate auth app

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@ if [[ $CREATE_SUPERUSER ]];
 then
   python manage.py migrate --noinput
   python manage.py collectstatic --noinput
+  python manage.py migrate auth
 #  python manage.py createsuperuser --no-input
 fi
